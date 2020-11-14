1bb46c011863a5415a12d5d5e6773f79270bd459
63070233 <73633482+63070233@users.noreply.github.com>
2020-11-14 14:06:57 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -14,11 +14,6 @@ $row = mysqli_fetch_assoc($result);
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-<div class="form">
-<p><a href="dashboard.php">Dashboard</a> 
-| <a href="insert.php">Insert New Record</a> 
-| <a href="logout.php">Logout</a></p>
-<h1>Update Record</h1>
 <?php
 $status = "";
 if(isset($_POST['new']) && $_POST['new']==1)
