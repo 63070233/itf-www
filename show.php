7e5e2fa05647b1ce0f28c5f847b96b0fef809a47
63070233 <73633482+63070233@users.noreply.github.com>
2020-11-14 13:55:23 +0700

    Update show.php

--- show.php
+++ show.php
@@ -34,8 +34,8 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    <td><?php echo $Result['ลบ'];?></td>
-    <td><a href="edit.php?id=<?php echo $Result['แก้ไข']; ?>">Edit</a></td>
+    <td><?php echo $Result['id'];?></td>
+    <td><a href="edit.php?id=<?php echo $Result['id']; ?>">Edit</a></td>
   </tr>
 <?php
 }
