a1a05513f5818b975f96f53c237f13e5fa19feef
63070233 <73633482+63070233@users.noreply.github.com>
2020-11-14 13:25:55 +0700

    Create testd.php

---
+++ testd.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf-nattapong.mysql.database.azure.com', 'it63070233@itf-nattapong', 'WGFuhe86', 'ITFLAB', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">ลบ </div></th>
+    <th width="150"> <div align="center">แก้ไข </div></th>
+  </tr>
+<?php
+$count=1;
+$sel_query="Select * from new_record ORDER BY id desc;";
+$result = mysqli_query($con,$sel_query);
+while($row = mysqli_fetch_assoc($result)) { ?>
+<tr><td align="center"><?php echo $count; ?></td>
+<td align="center"><?php echo $row["name"]; ?></td>
+<td align="center"><?php echo $row["age"]; ?></td>
+<td align="center">
+<a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
+</td>
+<td align="center">
+<a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a>
+</td>
+</tr>
+<?php $count++; } ?>
+mysqli_close($conn);
+?>
+</div>
+</body>
+</html>
