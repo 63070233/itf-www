a8c639ce2f81d7f426a66b65d12e68d07e0d4872
63070233 <73633482+63070233@users.noreply.github.com>
2020-11-18 21:06:18 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,43 @@
+<html>
+<head>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
+</head>
+<body>
 <?php
-require('db.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM new_record WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: show.php"); 
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf-nattapong.mysql.database.azure.com', 'it63070233@itf-nattapong', 'WGFuhe86', 'ITFLAB', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$sql = "DELETE FROM guestbook WHERE name='$name'";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "<center>Delete Successfully</center>";
+  } else {
+    echo "<center>Error: </center>" . $sql . "<br>" . mysqli_error($conn);
+  }
+
+mysqli_close($conn);
 ?>
+
+<div class="container">
+  <center><a href="show.php"><input type="button" value="Show" class="btn btn-info" class="ex1"></a></center>
+</div>
+</body>
+</html>
