e3f650f440519ec35971cd282f3f5863122e8364
63070233 <73633482+63070233@users.noreply.github.com>
2020-11-19 17:27:45 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,3 +1,18 @@
+<html>
+<head>
+<title>ITF Lab insert</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
